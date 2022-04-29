7026ed1cb203443f7e4c681b1cf028b994ca5baa
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2022-04-29 18:02:58 +0200

    Update YOLOR.py

--- YOLOR.py
+++ YOLOR.py
@@ -49,6 +49,7 @@ def detect(source = SOURCE, names = NAMES):
 
     # Load model
     model = Darknet(cfg, imgsz)
+    print (weights)
     model.load_state_dict(torch.load(weights[0], map_location=device)['model'])
     # model = attempt_load(weights, map_location=device)  # load FP32 model
     # imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
