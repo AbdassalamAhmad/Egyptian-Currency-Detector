2244fbb70c06b9bb30c29e838f089f4288e373f2
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2022-04-30 23:46:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -34,6 +34,10 @@ if __name__ == '__main__':
 
 
     download_data()
+
+    # get the size of file
+    size = os.path.getsize('./best_overall.pt') 
+    print('Size of file is', size, 'bytes')
     # Load model
     model = Darknet('./yolor_p6_custom.cfg', 640)
     #model = Darknet(cfg, imgsz).cuda()
