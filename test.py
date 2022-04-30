b58d223183f1f0f062d05700819b9b3b36c1a2bd
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2022-05-01 00:23:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,23 @@ from models.models import *
 #taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
 import requests
 
+ 
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    #CHUNK_SIZE = 32768
+    CHUNK_SIZE = 327680000
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
 
@@ -18,24 +35,7 @@ def download_file_from_google_drive(id, destination):
         params = { 'id' : id, 'confirm' : token }
         response = session.get(URL, params = params, stream = True)
 
-    save_response_content(response, destination)    
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-
+    save_response_content(response, destination)   
 # def download_data():
     
 
